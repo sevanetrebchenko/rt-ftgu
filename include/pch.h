3efd9643d191d9a34effdd0d037e020d7c43869a
seva.netrebchenko <seva.netrebchenko@gmail.com>
2021-05-03 23:45:27 -0700

    Initial project setup.

---
+++ include/pch.h
@@ -0,0 +1,40 @@
+
+#ifndef RT_PCH_H
+#define RT_PCH_H
+
+// Standard includes
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include <array>
+#include <bitset>
+#include <string>
+#include <queue>
+#include <list>
+#include <stdexcept>
+#include <utility>
+#include <atomic>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+#include <functional>
+#include <memory>
+#include <algorithm>
+#include <mutex>
+#include <thread>
+#include <cstdio>
+#include <variant>
+
+// Third-party
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/transform.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
+#include <glm/gtc/random.hpp>
+
+#include <tiny_obj_loader.h>
+
+
+#endif //RT_PCH_H
